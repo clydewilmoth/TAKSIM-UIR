651ff395a59ce51debbb65acbcd7ae34f983942c
selim <selimyag.eser@gmail.com>
2025-02-17 21:47:07 +0100

    Added index style default font family to Lora

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 import Layout from "./components/Layout/Layout";
 import Home from "./components/pages/Home/Home";
 import About from "./components/pages/About/About";
