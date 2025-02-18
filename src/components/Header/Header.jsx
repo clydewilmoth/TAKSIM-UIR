048730bf3a37e9f00575b4864c9a57de9743ddf0
selim <selimyag.eser@gmail.com>
2025-02-18 04:32:50 +0100

    Removed see through and added home Link to logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,35 +3,34 @@ import logo from "../../assets/logo.svg";
 import menu from "../../assets/menu.svg";
 import language from "../../assets/language.svg";
 import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
-  const [seeThrough, setSeeThrough] = useState(false);
   const [big, setBig] = useState("18vmax");
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      setSeeThrough(window.scrollY > 1 ? true : false);
       setBig(window.scrollY > 1 ? "0vmax" : "18vmax");
     });
 
     return () =>
       window.removeEventListener("scroll", () => {
-        setSeeThrough(window.scrollY > 1 ? true : false);
         setBig(window.scrollY > 1 ? "0vmax" : "18vmax");
       });
   }, []);
 
   return (
-    <div
-      className={css.header}
-      style={seeThrough === false ? { opacity: 1.0 } : { opacity: 0.85 }}
-    >
+    <div className={css.header}>
       <div className={css.logoWrapper}>
-        <img
-          src={logo}
-          className={css.logo}
-          style={big === "18vmax" ? { height: "12vmax" } : { height: "5vmax" }}
-        />
+        <Link to="/">
+          <img
+            src={logo}
+            className={css.logo}
+            style={
+              big === "18vmax" ? { height: "12vmax" } : { height: "5vmax" }
+            }
+          />
+        </Link>
       </div>
       <div className={css.nav}>
         <img src={menu} className={css.menu} />
