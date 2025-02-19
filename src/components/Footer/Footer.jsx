12d829d33bbeaba64de0f50e5d635c7524db3b9d
selim <selimyag.eser@gmail.com>
2025-02-19 01:04:21 +0100

    Initial Commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import css from "./styles.module.css";
+
+function Footer() {
+  return <>Footer</>;
+}
+
+export default Footer;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
+import Footer from "../Footer/footer";
 import "../../i18n";
 
 function Layout() {
@@ -7,6 +8,7 @@ function Layout() {
     <div id="layout">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
