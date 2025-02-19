f6c3bf05b56604bb6f57e84d0fb0ef2f48fdbda3
selim <selimyag.eser@gmail.com>
2025-02-19 04:53:07 +0100

    Footer done for now

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,55 @@
 import css from "./styles.module.css";
+import { useTranslation } from "react-i18next";
+import Phone from "../../assets/phone.svg";
+import Mail from "../../assets/mail.svg";
+import Instagram from "../../assets/instagram.svg";
 
 function Footer() {
-  return <>Footer</>;
+  const { t, i18n } = useTranslation();
+
+  return (
+    <div className={css.footer}>
+      <div className={css.wrapper}>
+        <div className={css.dataPolicy}>
+          <div className={css.heading}>{t("datenrichtlinien")}</div>
+          <div className={css.privacyPolicy}>{t("datenschutz")}</div>
+          <div className={css.legalNotice}>{t("impressum")}</div>
+        </div>
+        <div className={css.contact}>
+          <div className={css.heading}>{t("kontakt")}</div>
+          <div className={css.phoneWrapper}>
+            <a href="tel:+4962127939">
+              <img src={Phone} className={css.phone} />
+            </a>
+            +49 621 27939
+          </div>
+          <div className={css.mailWrapper}>
+            <a href="mailto:info@taksim-backwaren.de">
+              <img src={Mail} className={css.mail} />
+            </a>
+            info@taksim-
+            <br />
+            backwaren.de
+          </div>
+          <div className={css.instagramWrapper}>
+            <a
+              href="https://www.instagram.com/taksimbaklavalari/"
+              target="_blank"
+            >
+              <img src={Instagram} className={css.instagram} />
+            </a>
+            taksimbaklavalari
+          </div>
+        </div>
+        <div className={css.brand}>
+          <div className={css.name}>Taksim </div>Baklavacı{" "}
+          <div className={css.since}>
+            {i18n.language === "tr" ? "1979" + t("seit") : t("seit") + " 1979"}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,10 +2,11 @@ import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/footer";
 import "../../i18n";
+import css from "./styles.module.css";
 
 function Layout() {
   return (
-    <div id="layout">
+    <div className={css.layout}>
       <Header />
       <Outlet />
       <Footer />
